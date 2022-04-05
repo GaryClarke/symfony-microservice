9ad7d7174d0fdddc3e6f405c1d4ba1b265caee1a
Gary Clarke <garyclarke1974@gmail.com>
2022-04-05 20:00:38 +0100

    Create lowest-price endpoint

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ProductsController
 {
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(int $id): Response
+    {
+        dd($id);
+    }
 
+
+
+
+
+
+
+
+    #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions()
+    {
+
+    }
 }
