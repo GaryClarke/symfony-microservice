8879b5395e92e08d704c3c6e25598efb0dcc8fc9
Gary Clarke <garyclarke1974@gmail.com>
2022-04-06 11:50:23 +0100

    Complete basic route and responses

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,15 +2,35 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController
 {
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
-    public function lowestPrice(int $id): Response
+    public function lowestPrice(Request $request, int $id): Response
     {
-        dd($id);
+        if ($request->headers->has('force_fail')) {
+
+            return new JsonResponse(
+                ['error' => 'Promotions Engine failure message'],
+                $request->headers->get('force_fail')
+            );
+        }
+
+        return new JsonResponse([
+            'quantity' => 5,
+            'request_location' => 'UK',
+            'voucher_code' => 'OU812',
+            'request_date' => '2022-04-04',
+            'product_id' => $id,
+            'price' => 100,
+            'discounted_price' => 50,
+            'promotion_id' => 3,
+            'promotion_name' => 'Black Friday half price sale'
+        ], 200);
     }
 
 
