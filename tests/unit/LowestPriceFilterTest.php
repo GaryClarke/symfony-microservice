b390fc9d67ab3ae61ea5a7870b30b48028ab6754
Gary Clarke <garyclarke1974@gmail.com>
2022-04-30 16:05:02 +0100

    Complete test setup

--- tests/unit/LowestPriceFilterTest.php
+++ tests/unit/LowestPriceFilterTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\unit;
 
+use App\DTO\LowestPriceEnquiry;
+use App\Entity\Promotion;
 use App\Filter\LowestPriceFilter;
 use App\Tests\ServiceTestCase;
 
@@ -11,10 +13,11 @@ class LowestPriceFilterTest extends ServiceTestCase
     public function lowest_price_promotions_filtering_is_applied_correctly(): void
     {
         // Given
+        $enquiry = new LowestPriceEnquiry();
+
+        $promotions = $this->promotionsDataProvider();
 
         $lowestPriceFilter = $this->container->get(LowestPriceFilter::class);
-
-        dd($lowestPriceFilter);
 
         // When
         $filteredEnquiry = $lowestPriceFilter->apply($enquiry, ...$promotions);
@@ -23,5 +26,28 @@ class LowestPriceFilterTest extends ServiceTestCase
         $this->assertSame(100, $filteredEnquiry->getPrice());
         $this->assertSame(50, $filteredEnquiry->getDiscountedPrice());
         $this->assertSame('Black Friday half price sale', $filteredEnquiry->getPromotionName());
+    }
+
+    public function promotionsDataProvider(): array
+    {
+        $promotionOne = new Promotion();
+        $promotionOne->setName('Black Friday half price sale');
+        $promotionOne->setAdjustment(0.5);
+        $promotionOne->setCriteria(["from" => "2022-11-25", "to" => "2022-11-28"]);
+        $promotionOne->setType('date_range_multiplier');
+
+        $promotionTwo = new Promotion();
+        $promotionTwo->setName('Voucher OU812');
+        $promotionTwo->setAdjustment(100);
+        $promotionTwo->setCriteria(["code" => "OU812"]);
+        $promotionTwo->setType('fixed_price_voucher');
+
+        $promotionThree = new Promotion();
+        $promotionThree->setName('Buy one get one free');
+        $promotionThree->setAdjustment(0.5);
+        $promotionThree->setCriteria(["minimum_quantity" => 2]);
+        $promotionThree->setType('even_items_multiplier');
+
+        return [$promotionOne, $promotionTwo, $promotionThree];
     }
 }
