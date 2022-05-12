77076fe9c2876642178cc3662c3c60c39519f2b3
Gary Clarke <garyclarke1974@gmail.com>
2022-05-12 14:39:44 +0100

    Complete happy path functionality

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -17,25 +17,24 @@ class LowestPriceFilter implements PromotionsFilterInterface
     : PromotionEnquiryInterface
     {
         $price = $enquiry->getProduct()->getPrice();
+        $enquiry->setPrice($price);
         $quantity = $enquiry->getQuantity();
         $lowestPrice = $quantity * $price;
 
-        // Loop over the promotions
         foreach ($promotions as $promotion) {
-            // Run the promotions' modification logic against the enquiry
-            // 1. check does the promotion apply e.g. is it in date range / is the voucher code valid?
-            // 2. Apply the price modification to obtain a $modifiedPrice (how?)
+
             $priceModifier = $this->priceModifierFactory->create($promotion->getType());
 
             $modifiedPrice = $priceModifier->modify($price, $quantity, $promotion, $enquiry);
-            // 3. check IF $modifiedPrice < $lowestPrice
-            // 1. Save to Enquiry properties
-            // 2. Update $lowestPrice
 
-            $enquiry->setDiscountedPrice(250);
-            $enquiry->setPrice(100);
-            $enquiry->setPromotionId(3);
-            $enquiry->setPromotionName('Black Friday half price sale');
+            if($modifiedPrice < $lowestPrice) {
+
+                $enquiry->setDiscountedPrice($modifiedPrice);
+                $enquiry->setPromotionId($promotion->getId());
+                $enquiry->setPromotionName($promotion->getName());
+
+                $lowestPrice = $modifiedPrice;
+            }
         }
 
         return $enquiry;
--- tests/unit/LowestPriceFilterTest.php
+++ tests/unit/LowestPriceFilterTest.php
@@ -20,6 +20,8 @@ class LowestPriceFilterTest extends ServiceTestCase
         $enquiry = new LowestPriceEnquiry();
         $enquiry->setProduct($product);
         $enquiry->setQuantity(5);
+        $enquiry->setRequestDate('2022-11-27');
+        $enquiry->setVoucherCode('OU812');
 
         $promotions = $this->promotionsDataProvider();
 
